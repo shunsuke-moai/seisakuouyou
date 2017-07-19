37886b2aa052f0cc86721db0322c6a8d88a7584b
DESKTOP-POH68L3\GD-lab <g1527035@st.t-kougei.ac.jp>
2017-07-19 18:36:43 +0900

    初回ですよ

---
+++ New Unity Project 1/Assets/Model/EnemyController.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+    public enum Color
+    {
+        Red,
+        Blue,
+        Green,
+        Yellow,
+        Any,
+        All
+    }
+
+    public Color color = new Color();
+
+	// Use this for initialization
+    public float moveSpeed;
+	void Start () {
+        moveSpeed = -0.05f;
+        switch(transform.tag)
+        {
+            case "Enemy_Red":
+                //どの色か控えをとる
+                color = Color.Red;
+                break;
+
+            case "Enemy_Blue":
+                //どの色か控えをとる
+                color = Color.Blue;
+                break;
+
+            case "Enemy_Green":
+                //どの色か控えをとる
+                color = Color.Green;
+                break;
+
+            case "Enemy_Yellow":
+                //どの色か控えをとる
+                color = Color.Yellow;
+                break;
+
+            case "Enemy_Any":
+                //どの色か控えをとる
+                color = Color.Any;
+                break;
+
+            case "Enemy_All":
+                //どの色か控えをとる
+                color = Color.All;
+                break;
+            default:
+                color = Color.All;
+                break;
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0, 0, this.moveSpeed);
+
+        //一定距離に進入してきたかどうか
+        if (transform.position.z < -5.0)
+        {
+            //ボタンが押された
+            if (Input.GetKeyDown(KeyCode.Keypad1))
+            {
+                //プレイヤーと色が対応している
+                if(DestoryCheck())
+                {
+                    Destroy(gameObject);
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.Keypad2))
+            {
+                if (DestoryCheck())
+                {
+                    Destroy(gameObject);
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.Keypad3))
+            {
+                if (DestoryCheck())
+                {
+                    Destroy(gameObject);
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.Keypad4))
+            {
+                if (DestoryCheck())
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+
+        if (transform.position.z < -8.0f)
+        {
+            Destroy(gameObject);
+        }
+	}
+    bool DestoryCheck()//引数でたたいたプレイヤーを渡す
+    {
+        if (color == Color.All)
+        {
+            return true;
+        }
+        if (color == Color.Any)
+        {
+            return true;
+        }
+        if (color == Color.Red)//&& たたいたプレイヤー==Color.Red
+        {
+            return true;
+        }
+        if (color == Color.Blue)//&& たたいたプレイヤー==Color.Blue
+        {
+            return true;
+        }
+        if (color == Color.Green)//&& たたいたプレイヤー==Color.Green
+        {
+            return true;
+        }
+        if (color == Color.Yellow)//&& たたいたプレイヤー==Color.Yellow
+        {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ New Unity Project 1/Assets/Source/Enemygenerator.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemygenerator : MonoBehaviour {
+    public GameObject Enemy_RedPrefab;
+    public GameObject Enemy_BluePrefab;
+    public GameObject Enemy_GreenPrefab;
+    public GameObject Enemy_YellowPrefab;
+    public GameObject Enemy_AnyPrefab;
+    public GameObject Enemy_AllPrefab;
+
+    float span = 1.0f;
+    float delta = 0;
+
+    float[] EnemyPos = new float[4] {-1.5f, -0.5f, 0.5f, 1.5f};
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.delta += Time.deltaTime;
+
+        //1秒ごとに敵を出します
+        if (this.delta > this.span)
+        {
+            this.delta = 0;
+            GameObject Enemy = null;
+            int dice = Random.Range(0, 6);
+            switch (dice)
+            {
+                case 0:
+                    Enemy = Instantiate(Enemy_AnyPrefab) as GameObject;
+                    break;
+
+                case 1:
+                    Enemy = Instantiate(Enemy_RedPrefab) as GameObject;
+                    break;
+
+                case 2:
+                    Enemy = Instantiate(Enemy_BluePrefab) as GameObject;
+                    break;
+
+                case 3:
+                    Enemy = Instantiate(Enemy_GreenPrefab) as GameObject;
+                    break;
+
+                case 4:
+                    Enemy = Instantiate(Enemy_YellowPrefab) as GameObject;
+                    break;
+
+                case 5:
+                    for (int i = 0; i < 4; i++)
+                    {
+                        Enemy = Instantiate(Enemy_AllPrefab) as GameObject;
+                        Enemy.transform.position = new Vector3(EnemyPos[i], 0.5f, 0.0f);
+                    }
+                    break;
+            }
+            if (dice != 5)
+            {
+                int x = Random.Range(0, 4);
+                Enemy.transform.position = new Vector3(EnemyPos[x], 0.5f, 0.0f);
+            }
+        }
+	}
+}
